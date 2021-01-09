9ca671d1aeb06effd5644484d2285f520c07342e
Babak Boroujerdi Far <babakbf@gmail.com>
2021-01-09 14:45:05 -0500

    weather module

--- weather_module.py
+++ weather_module.py
@@ -11,6 +11,8 @@ import datetime
 import os.path
 import feedparser
 import requests, json 
+import pandas as pd
+
 
 class Weather():
 
@@ -44,7 +46,7 @@ class Weather():
             url=self.base_url+"weather?lat={lat}&lon={lon}&appid={API_key}&units={units}".format(lat=self.latitude,lon=self.longitude(), API_key=self.openweathermap_appkey,units=self.units)
         return url
     
-    def TodayWeather(self):
+    def WeatherInf(self):
         objWeather=Weather()
         url=objWeather.GetWeatherUrl('D')
         print(url)
@@ -53,30 +55,40 @@ class Weather():
         dict_current = {}
         if "current" in dict_weather:
             cur_weather=dict_weather["current"]
-            cur_feels=cur_weather["feels_like"]
-            cur_humidity=cur_weather["humidity"]
-            cur_temp=cur_weather["temp"]
-            cur_dew=cur_weather["dew_point"]
-            cur_pressure=cur_weather["pressure"]
-            cur_visibility=cur_weather["visibility"]
-            cur_wind_speed=cur_weather["wind_speed"]
-            today_icon=dict_weather["current"]["weather"][0]["icon"]
+            dict_current["cur_feels_Like"]=round(cur_weather["feels_like"])
+            dict_current["cur_humidity"]=str(round(cur_weather["humidity"])) + " %"
+            dict_current["cur_temp"]=round(cur_weather["temp"])
+            dict_current["cur_dew"]=round(cur_weather["dew_point"])
+            dict_current["cur_pressure"]=cur_weather["pressure"]
+            dict_current["cur_visibility"]=str(round(cur_weather["visibility"]/1000)) +" KM"
+            dict_current["cur_wind_speed"]=str(round(cur_weather["wind_speed"]*3.6))+ " KM/H"
             today_sunset=cur_weather["sunset"]
             today_sunrise=cur_weather["sunrise"]
-            sunrise_utc_time = datetime.datetime.fromtimestamp(today_sunset, timezone.utc)
+            sunrise_utc_time = datetime.datetime.fromtimestamp(today_sunrise, timezone.utc)
             sunrise_local_time = sunrise_utc_time.astimezone()
-            sunrise=sunrise_local_time.strftime("%H:%M")
-            sunset_utc_time = datetime.datetime.fromtimestamp(today_sunrise, timezone.utc)
+            dict_current["today_sunrise"]=sunrise_local_time.strftime("%H:%M")
+            sunset_utc_time = datetime.datetime.fromtimestamp(today_sunset, timezone.utc)
             sunset_local_time = sunset_utc_time.astimezone()
-            sunset=sunset_local_time.strftime("%H:%M")
+            dict_current["today_sunset"]=sunset_local_time.strftime("%H:%M")
             #GUI.icon_label.photo = today_icon  
-            icon_path = 'weather_icons/'
+            today_icon=dict_weather["current"]["weather"][0]["icon"]
+            dict_current["today_icon"]= self.icon_path+today_icon+self.icon_ext
+            today_weather_id=dict_weather["current"]["weather"][0]["id"]
+            dict_current["cur_desc"]=objWeather.GetWeatherDesc(today_weather_id)
+            print(dict_current)
         #     today_icon += '.png'
         #     icon_path += today_icon
         #     print(icon_path)
         #     #icon = PhotoImage(file='weather_icons/04n.png')  
         #     GUI.icon_label.configure(image=icon)
         #     GUI.icon_label.photo = icon  
+    def GetWeatherDesc(self, weather_id):
+        df=pd.read_csv("weather_conditions.csv")
+        df_new=df.query('ID=='+str(weather_id))
+        weather_desc=''
+        for item in df_new["Description"].values:
+            weather_desc += item
+        return str(weather_desc)
 
 a=Weather()
-a.TodayWeather()
+a.WeatherInf()
