2f8737e16e19e74d952495cde5e5392507f36d32
Babak Boroujerdi Far <babakbf@gmail.com>
2021-01-11 20:18:20 -0500

    new module

--- news_module.py
+++ news_module.py
@@ -2,24 +2,19 @@
 """
 Created on Tue Jan  5 21:00:28 2021
 
-@author: babak
+@author: Babak Boroujerdi Far
 """
-import time
-from time import strftime
-from datetime import date, timedelta, datetime, timezone
-import datetime
-import os.path
 import feedparser
 import requests, json 
 import pandas as pd
 
 
-class News(NewsType,NumberOfRows):
+class News():
 
     def __init__(self):
         self.number_of_rows_defult=5
 
-    def GetNews(self,NewsType):
+    def GetNewsUrl(self,NewsType):
         # News Type T: Technology Science BI:BBC Persian Iran  R: Radio Farda CW: 
         #    CBC World  CC: CBC CANADA   B: BBC Perisan
         
@@ -33,34 +28,37 @@ class News(NewsType,NumberOfRows):
             url_news='https://www.radiofarda.com/api/zrttpoeuoupo'
         elif NewsType=='CW': # CBC World (English)
             url_news='https://www.cbc.ca/cmlink/rss-world'
-        elif NewsType=='CW': # Radio Farda (Perisan)
-            url_news='https://www.cbc.ca/cmlink/rss-canada'
-        else: # Current
-            # https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-            url=self.base_url+"weather?lat={lat}&lon={lon}&appid={API_key}&units={units}".format(lat=self.latitude,lon=self.longitude(), API_key=self.openweathermap_appkey,units=self.units)
-        return url
+        elif NewsType=='T': # Technology Science
+            url_news='https://www.sciencedaily.com/rss/all.xml'
+        elif NewsType=='S': # Technology Science
+            url_news='http://feeds.feedburner.com/BreakingScienceNews?format=xml'
+        elif NewsType=='TE': # Technology Science
+            url_news='https://www.sciencedaily.com/rss/top/technology.xml'
+        return url_news
     
 
-url_radiofarda='https://www.radiofarda.com/api/zrttpoeuoupo'
-url_cbc_world='https://www.cbc.ca/cmlink/rss-world'
+#url_radiofarda='https://www.radiofarda.com/api/zrttpoeuoupo'
+#url_cbc_world='https://www.cbc.ca/cmlink/rss-world'
     
-    def GetTopNews(self):
-            dict_iran = feedparser.parse(url_radiofarda)
+    def GetTopNews(self,NewsType,NumberofRows):
+        objNews = News()
+        url = objNews.GetNewsUrl(NewsType)
+        dict_news = feedparser.parse(url)
         #
         j=5
         title_new_list = []
-        if len(dict_iran)<5:
-            j=len(dict_iran)
+        if len(dict_news)<5:
+            j=len(dict_news)
             
         for i in range(0, j):
-            title = dict_iran['entries'][i]["title"]
+            title = dict_news['entries'][i]["title"]
             title_new = title
             #if(len(title) >= 50):
             #    title_new += title[:50] + '-\n' + title[50:]
             
             title_new_list.append(title_new)       
-        
-        return dict_activity
+        print(title_new_list)
+        return title_new_list
     
-a=Weather()
-a.WeatherInf()
+a=News()
+a.GetTopNews('TE',5)
